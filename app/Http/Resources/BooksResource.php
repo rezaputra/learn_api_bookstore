5331719eaa4c43857779977a374fb1bc987c0e1a
reza <rezaputramardiansyah@gmail.com>
2022-01-24 06:12:47 +0700

    third commit

---
+++ app/Http/Resources/BooksResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BooksResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => (string)$this->id,
+            'type' => 'Books',
+            'attributes' => [
+                'name' => $this->name,
+                'author' => $this->author,
+                'description' => $this->description,
+                'publication_year' => $this->publication_year
+            ]
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
+use App\Http\Controllers\BooksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware('auth:api')->prefix('v1')->group(function(){
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Route::get('authors/{author}', [AuthorsController::class,'show']);
+    Route::apiResource('/authors', AuthorsController::class);
+    Route::apiResource('/books', BooksController::class);
 });
